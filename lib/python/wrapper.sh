bad4eeb3035288d31aeac0a5306ea46febfff290
mlw214 <miller.wilt@gmail.com>
2014-03-28 21:57:04 -0400

    Forgot to actually send bounds to webcam process

--- lib/python/wrapper.sh
+++ lib/python/wrapper.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./webcam.py 1> >(ffmpeg -f rawvideo -pix_fmt bgr24 -r 10 -s 320x240 -i - -an -f flv -metadata streamName=myStream tcp://0.0.0.0:6666 2>/dev/null)
+lib/python/webcam.py 1> >(ffmpeg -f rawvideo -pix_fmt bgr24 -r 10 -s 320x240 -i - -an -f flv -metadata streamName=myStream tcp://0.0.0.0:6666 2>/dev/null)
