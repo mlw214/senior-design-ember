634077a6f7d37ab1bc9c6cb3270c1ab52c40818d
mlw214 <miller.wilt@gmail.com>
2014-04-13 18:21:08 -0400

    Bug fixes

--- lib/python/wrapper.sh
+++ lib/python/wrapper.sh
@@ -5,4 +5,4 @@
 # wrapper.sh
 
 # Code glue to get webcam.py to interface with FFmpeg
-lib/python/webcam.py 1> >(ffmpeg -f rawvideo -pix_fmt bgr24 -r 10 -s 320x240 -i - -an -f flv -metadata streamName=myStream tcp://0.0.0.0:6666 2>/dev/null)
+lib/python/webcam.py 1> >(ffmpeg -f rawvideo -pix_fmt bgr24 -r 1 -s 320x240 -i - -an -f flv -metadata streamName=myStream tcp://0.0.0.0:6666 2>/dev/null)
